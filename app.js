874494db90d93e20d72face10498cacf4af0c71e
shrutishukla11 <102374779+shrutishukla11@users.noreply.github.com>
2024-09-02 23:22:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,39 @@ const app_id = "38418589";
 const app_key = "6e344a72b040c0a8776bb69517005b9d";
 
 var button = document.querySelector("button");
-console.log(button);
 
 const get_recipe = async () => {
   var input = document.querySelector("input");
 
-  console.log(input);
   const response = await fetch(
     `https://api.edamam.com/api/recipes/v2?type=public&q=${input.value}&app_id=${app_id}&app_key=${app_key}`
   );
   var data = await response.json();
 
-  
-  console.log(data);
-  //   data = data.hits;
   var cards = document.querySelector(".Cards-section");
   cards.innerHTML = "";
   for (let i = 0; i < data.hits.length; i++) {
+    const recipe = data.hits[i].recipe;
+    
+    // Create a description by joining the first few ingredient lines
+    const description = recipe.ingredientLines.slice(0, 3).join(', ') + '...';
+
     cards.innerHTML += `
-  <div class="card">
-  <div class="card__body">
-    <img
-      src="${data.hits[i].recipe.image}"
-      alt=""
-      class="card__image"
-    />
-    <h2 class="card__title">${data.hits[i].recipe.label}</h2>
-    <p class="card__description">
-      Take your boring salads up a knotch. This recipe is perfect for
-      lunch and only contains 5 ingredients!
-    </p>
-  </div>
-  <a href="${data.hits[i].recipe.url}" class="card__btn">View Recipe</a>
-</div>
-  `;
+    <div class="card">
+      <div class="card__body">
+        <img
+          src="${recipe.image}"
+          alt=""
+          class="card__image"
+        />
+        <h2 class="card__title">${recipe.label}</h2>
+        <p class="card__description">
+          ${description}
+        </p>
+      </div>
+      <a href="${recipe.url}" class="card__btn">View Recipe</a>
+    </div>
+    `;
   }
 };
 
